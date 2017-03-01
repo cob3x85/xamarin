cab63f12033dbcc2f8f317a636569cad0592d1dd
Carlos Ortiz Bravo <cob3x85@outlook.com>
2017-02-28 23:23:24 -0600

    Add the OnButton Clicked code... partial. Just Download from Blob

--- Diploma.StorageDownload/Diploma.StorageDownload/MainActivity.cs
+++ Diploma.StorageDownload/Diploma.StorageDownload/MainActivity.cs
@@ -7,6 +7,7 @@ using Microsoft.WindowsAzure.Storage.Table;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using XamarinDiplomado.Participants.Startup;
+using System.IO;
 
 namespace Diploma.StorageDownload
 {
@@ -31,9 +32,30 @@ namespace Diploma.StorageDownload
       imagen = FindViewById<ImageView>(Resource.Id.imagen);
       btnDescargar = FindViewById<Button>(Resource.Id.btnDescargar);
 
+      btnDescargar.Click += DescargarImagen;
 
       Startup startup = new Startup("Carlos Ortiz Bravo", "cob3x85@outlook.com", 3, 1);
       startup.Init();
+    }
+
+    private async void DescargarImagen(object sender, System.EventArgs e)
+    {
+      string carpeta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+      string archivoLocal = "Foto.jpg";
+      string ruta = System.IO.Path.Combine(carpeta, archivoLocal);
+
+      CloudStorageAccount cuentaAlmacenamiento = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=cobxamarin;AccountKey=wWWEUzzSVVaHlbXHVs8PWE7gk3ndkEP8Ft8JOxqjHpO1QAvVenyKDXZ/T5yfd4yLZTfwUD4TMfWlev9f63jSUg==");
+      CloudBlobClient clienteBlob = cuentaAlmacenamiento.CreateCloudBlobClient();
+      CloudBlobContainer contenedorBlob = clienteBlob.GetContainerReference("lab01");
+      CloudBlockBlob recursoBlob = contenedorBlob.GetBlockBlobReference("Foto.jpg");
+
+      var stream = File.OpenWrite(ruta);
+      await recursoBlob.DownloadToStreamAsync(stream);
+      Android.Net.Uri rutaImagen = Android.Net.Uri.Parse(ruta);
+      imagen.SetImageURI(rutaImagen);
+
+
+      //TableEntity Pendning
     }
   }
 }
