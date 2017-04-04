db791fb539c25fb7e305a99b746c3ef611ab16dd
cob3x85 <cob3x85@outlook.com>
2017-04-03 23:36:55 -0500

    Agregamos Reto 4 Start

---
+++ XamarinChampionship/Reto4/Reto4/Services/ServiceHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Reto4.Services
+{
+  public class ServiceHelper
+  {
+    MobileServiceClient clienteServicio = new MobileServiceClient(@"http://xamarinchampions.azurewebsites.net");
+
+    private IMobileServiceTable<TorneoItem> _TorneoItemTable;
+
+    public async Task BuscarRegistros(string Correo)
+    {
+      _TorneoItemTable = clienteServicio.GetTable<TorneoItem>();
+      List<TorneoItem> items = await _TorneoItemTable.Where(
+        torneoItem => torneoItem.Email == Correo).ToListAsync();
+    }
+  }
+}
