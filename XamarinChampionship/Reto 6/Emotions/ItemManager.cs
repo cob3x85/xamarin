c138256898d0fa318a6d4abcc514935fe3c47d6d
cob3x85 <cob3x85@outlook.com>
2017-05-04 13:46:42 -0500

    Agregamos la conexion con Azure y creamos la tabla TodoItem

---
+++ XamarinChampionship/Reto 6/Emotions/ItemManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
+namespace Emotions
+{
+  public class ItemManager
+  {
+    static ItemManager defaultInstance = new ItemManager();
+    MobileServiceClient client;
+
+    IMobileServiceTable<TorneoItem> todoTable;
+
+    private ItemManager()
+    {
+      this.client = new MobileServiceClient(@"https://xamarinchampions.azurewebsites.net/");
+      this.todoTable = client.GetTable<TorneoItem>();
+    }
+
+    public ItemManager DefaultManager { get; set; }
+
+    public MobileServiceClient CurrentClient
+    {
+      get { return client; }
+    }
+
+    public async Task SaveTaskAsync(TorneoItem item)
+    {
+      if(item.Id == null)
+      {
+        await todoTable.InsertAsync(item);
+      }
+      else
+      {
+        await todoTable.UpdateAsync(item);
+      }
+    }
+  }
+}
