7ee5da890408917ffa4d3625494ad6db29ec2b94
cob3x85 <cob3x85@outlook.com>
2017-05-17 17:01:25 -0500

    Finish all instructions. Ready to test

--- XamarinChampionship/Reto 6/Reto6/CognitiveActivity.cs
+++ XamarinChampionship/Reto 6/Reto6/CognitiveActivity.cs
@@ -10,6 +10,7 @@ using Android.Views;
 using Android.Widget;
 using Emotions;
 using System.IO;
+using Plugin.Media.Abstractions;
 
 namespace Reto6
 {
@@ -19,7 +20,7 @@ namespace Reto6
 
     ItemManager manager;
     static Stream streamCopy;
-    string ResultEmotions = "Reto6 + MX + Carlos Ortiz Bravo";
+    string ResultadoEmociones = "Reto6 + MX + Carlos Ortiz Bravo";
     TextView txtResultado;
     Button btnRegistraResultados;
     Button btnAnalizaFoto;
@@ -34,7 +35,56 @@ namespace Reto6
 
       Button btnCamera = FindViewById<Button>(Resource.Id.btnCamara);
       btnAnalizaFoto = FindViewById<Button>(Resource.Id.btnAnalizarFoto);
-      //btnRegistraResultados
+      btnRegistraResultados = FindViewById<Button>(Resource.Id.btnRegistraResultados);
+      txtResultado = FindViewById<TextView>(Resource.Id.txtOutput);
+
+      btnRegistraResultados.Visibility = ViewStates.Invisible;
+      btnAnalizaFoto.Visibility = ViewStates.Invisible;
+      btnCamera.Click += BtnCamera_Click;
+      btnAnalizaFoto.Click += BtnAnalizaFoto_Click;
+      btnRegistraResultados.Click += BtnRegistraResultados_Click;
+
+    }
+
+    private void BtnRegistraResultados_Click(object sender, EventArgs e)
+    {
+      btnRegistraResultados.Visibility = ViewStates.Invisible;
+      Toast.MakeText(this, "Registrando tus resultados", ToastLength.Short).Show();
+      TorneoItem registro = new TorneoItem();
+      registro.DeviceId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+      registro.Email = "cob3x85@outlook.com";
+      registro.Reto = ResultadoEmociones;
+    }
+
+    private void BtnAnalizaFoto_Click(object sender, EventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private async void BtnCamera_Click(object sender, EventArgs e)
+    {
+      MediaFile file = null;
+      try
+      {
+        file = await ImageService.TakePicture(true);
+      }
+      catch (Android.OS.OperationCanceledException) { }
+      SetImageToControl(file);
+      btnAnalizaFoto.Visibility = ViewStates.Visible;
+    }
+    private void SetImageToControl(MediaFile file)
+    {
+      if (file == null)
+      {
+        return;
+      }
+      ImageView imgImage = FindViewById<ImageView>(Resource.Id.imageViewFoto);
+      imgImage.SetImageURI(Android.Net.Uri.Parse(file.Path));
+      var stream = file.GetStream();
+      streamCopy = new MemoryStream();
+      stream.CopyTo(streamCopy);
+      stream.Seek(0, SeekOrigin.Begin);
+      file.Dispose();
     }
 
   }
